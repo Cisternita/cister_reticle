2d48548d5d48d07a283f107f1aeef0c50705f0cf
Cisternita <88116214+Cisternita@users.noreply.github.com>
2022-02-09 23:18:43 +0100

    Updated main system

--- reticle.lua
+++ reticle.lua
@@ -1,8 +1,7 @@
 Citizen.CreateThread(function()
     while true do
       Wait(0)
-      if Reticle.ReticleVehicle == true then
-      -- MIRA EN COCHE
+      if Reticle.ReticleVehicle == true then   -- MIRA EN COCHE
        playerdist = GetEntityCoords()
        dist = GetDistanceBetweenCoords(playerdist - GetClosestVehicle())
       if dist < 20 then
@@ -19,32 +18,31 @@ Citizen.CreateThread(function()
   end
   end
   end)
-  
+
   Citizen.CreateThread(function()
       while true do
         Wait(0)
         if IsPedArmed(PlayerPedId(), 4) or IsPedArmed(PlayerPedId(), 1) or IsPedArmed(PlayerPedId(), 2) then
           
-          if Reticle.DisableRoll == true then
+          if Reticle.DisableRoll == true then --QUITAR EL RODAR
               DisableControlAction(25, 22)
             end
-            if Reticle.ReticleDisableOnMoving == true then
-    if IsControlPressed(0, 25) and IsControlPressed(0, 32) or IsControlPressed(0, 25) and IsControlPressed(0, 33) or IsControlPressed(0, 25) and IsControlPressed(0, 34) or IsControlPressed(0, 25) and IsControlPressed(0, 35) then
-     -- HideHudComponentThisFrame(14)
-      if Reticle.Recoil == true then
-    if IsPedShooting(PlayerPedId()) then
-        RecoilThirdPersonMultiplier(1.0,0.5)
-        --HideHudComponentThisFrame(14)
-      end
-    end
-    end
-    else
-      Wait(1000)
-    end
-  else
-    Wait(3000)
-    end
-  end
+                    if Reticle.ReticleDisableOnMoving == true then  --QUITAR LA RETÍCULA CUANDO CAMINAS, PERO TENRLA SI APUNTAS Y NO TE MUEVES
+                        if IsControlPressed(0, 25) and IsControlPressed(0, 32) or IsControlPressed(0, 25) and IsControlPressed(0, 33) or IsControlPressed(0, 25) and IsControlPressed(0, 34) or IsControlPressed(0, 25) and IsControlPressed(0, 35) then
+                        HideHudComponentThisFrame(14)
+                        end
+                    end
+                            if Reticle.Recoil == true then --AÑADIR RECOIL AL DISPARAR MIENTRAS TE MUEVES
+                                if IsControlPressed(0, 25) and IsControlPressed(0, 32) or IsControlPressed(0, 25) and IsControlPressed(0, 33) or IsControlPressed(0, 25) and IsControlPressed(0, 34) or IsControlPressed(0, 25) and IsControlPressed(0, 35) then
+                                    if IsPedShooting(PlayerPedId()) then
+                                        RecoilThirdPersonMultiplier(1.0,0.5)
+                                    end
+                                end
+                            end
+        else
+            Wait(1000)
+        end
+     end
   end)
   
   function RecoilThirdPersonMultiplier(ThirdPersonMultiplier,ThirdPersonAimingMultiplier)
